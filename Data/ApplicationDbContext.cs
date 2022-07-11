df25a8ed12a2b0957d0f79d3cb19aec1e94ba116
tesv <thiago.saraiva@gft.com>
2022-07-11 09:22:13 -0300

    AddRentMovie Model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ namespace APILocacao.Data
 {
     public class ApplicationDbContext: DbContext
     {
+        public DbSet<RentMovie> RentMovies{get;set;}
         public DbSet<Client> Clients{get;set;}
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ Models/RentMovie.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APILocacao.Models
+{
+    public class RentMovie
+    {
+        public int Id { get; set; }
+        public Client Clients { get; set; }    
+        //public Movie Movies { get; set; }    
+        public DateTime FinalDeliveryDate { get; set; }
+        public bool Rent { get; set; }
+        public decimal TotalRent { get; set; }
+        
+    }
+}
