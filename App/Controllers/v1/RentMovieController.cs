76dd1e1554eb6492ddcf8757a7c7c50f7e86304c
tesv <thiago.saraiva@gft.com>
2022-07-15 14:58:16 -0300

    Clean Controller

--- App/Controllers/v1/RentMovieController.cs
+++ App/Controllers/v1/RentMovieController.cs
@@ -30,11 +30,8 @@ namespace APILocacao.Controllers
         {
             if (ModelState.IsValid)
             {
-                var clientId = await _rentMovieRepository.GetClientAsync(rentMovieDTO.ClientId);
-                //var clientId = await _context.Clients.FirstOrDefaultAsync(cli => cli.CPF == rentMovieDTO.ClientId);
+                var clientId = await _rentMovieRepository.GetClientAsync(rentMovieDTO.ClientId);               
                 var movieId = await _rentMovieRepository.GetMovieAsync(rentMovieDTO.MovieId);
-               //.Movies.FirstOrDefaultAsync(mov => mov.Id == rentMovieDTO.MovieId);
-
                 if (clientId == null || movieId == null)
                 {                   
                     return NotFound("The client or movie does not exist in the database");
@@ -42,10 +39,8 @@ namespace APILocacao.Controllers
                 if (movieId.Amount <= 0)
                 {                    
                     return new ObjectResult("The chosen movie has already been rented ");
-                }              
-                                    
-                RentMovie rentMovie = new RentMovie();    
-                // rentMovie = _mapper.Map<RentMovie>(rentMovieDTO);                
+                }                                    
+                RentMovie rentMovie = new RentMovie();                                  
                 rentMovie.Clients = clientId;
                 rentMovie.Movies = movieId;                
                 rentMovie.FinalDeliveryDate = rentMovieDTO.FinalDeliveryDate;                
@@ -53,12 +48,8 @@ namespace APILocacao.Controllers
                 //business rule
                 rentMovie.ReturnMovie = false;
                 movieId.Amount -= 1;
-                var responseMovie = await _rentMovieRepository.SetAmountMovie(movieId);
-                //_context.Update(movieId);   
-                //await _context.SaveChangesAsync(); 
-                var responseDatabase = await _rentMovieRepository.AddRentMovie(rentMovie);         
-                //_context.Add(rentMovie);
-                //await _context.SaveChangesAsync();
+                var responseMovie = await _rentMovieRepository.SetAmountMovie(movieId);              
+                var responseDatabase = await _rentMovieRepository.AddRentMovie(rentMovie);             
                 Response.StatusCode = 201;
                 return new ObjectResult("The movie rental was successful");                      
             }
@@ -72,12 +63,7 @@ namespace APILocacao.Controllers
         public async Task<IActionResult> MovieReturn(ulong cpf)
         {
             RentMovie rentMovie = new();
-            var client = await _rentMovieRepository.GetRentMovie(cpf);
-            // var client = await _context.RentMovies
-            //              .Include(cli => cli.Clients)
-            //              .Include(mov => mov.Movies)
-            //              .Where(rent => rent.ReturnMovie == false)
-            //              .FirstOrDefaultAsync(cli => cli.Clients.CPF == cpf);                        
+            var client = await _rentMovieRepository.GetRentMovie(cpf);         
             if (client == null)
             {                
                 return Ok("The client has no one movie rented");
@@ -91,11 +77,9 @@ namespace APILocacao.Controllers
             client.ReturnMovie = true;
             await _rentMovieRepository.UpdateRentMovie(client);     
             var movie = await _rentMovieRepository.GetMovieAsync(client.Movies.Id);      
-            movie.Amount += 1;
-            // Movie movie = await _context.Movies.FirstOrDefaultAsync(mov => mov.Id == client.Movies.Id);          
+            //business rule
+            movie.Amount += 1;       
             await _rentMovieRepository.SetAmountMovie(movie);
-            // _context.Update(movie);
-            // await _context.SaveChangesAsync();
             return Ok("The movie has return with successfull");
         }
     }
